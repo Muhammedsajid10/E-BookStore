caf7d04171e9aaa4a002613f76b892773312faaf
muhammedsajid <muhammedsajid@q2kg.onmicrosoft.com>
2023-11-19 11:26:39 +0530

    second

--- BookStore/client/src/AdminLogin.jsx
+++ BookStore/client/src/AdminLogin.jsx
@@ -1,28 +1,18 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import './AdminLogin.css'; 
+import './AdminLogin.css';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import AdminNav from './AdminNav';
-import Modal from 'react-bootstrap/Modal'
+import Modal from 'react-bootstrap/Modal';
 
 const AdminLogin = () => {
-  //   const [firstName, setFirstName] = useState("");
-  //   const [secondName, setSecondName] = useState("");
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
   const [errorMessage, serErrorMessage] = useState("");
   const [showErrorModal, setShowErrorModal] = useState(false);
   const navigate = useNavigate();
-
-  //   const handleFirstName = (e) => {
-  //     setFirstName(e.target.value);
-  //   };
-
-  //   const handleSecondName = (e) => {
-  //     setSecondName(e.target.value);
-  //   };
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
@@ -44,58 +34,56 @@ const AdminLogin = () => {
     e.preventDefault();
     console.log(Email, Password);
     try {
-      const response = await axios.post('http://localhost:5000/compAdmin', { Email, Password })
+      const response = await axios.post('http://localhost:5000/compAdmin', { Email, Password });
       console.log(response.data);
-      localStorage.setItem('adninnDetails', response.data)
+      localStorage.setItem('adninnDetails', response.data);
       navigate('/home');
     } catch (error) {
       console.log("hhhhhhaaaa::", error.response.data);
       serErrorMessage("Invalid Email or Password");
-      handleShowErrorModal(); // Show the error modal
-
+      handleShowErrorModal();
     }
-
   };
 
   return (
     <div>
-     
-      <div className="form-container fade-in-animation">
-        {/* <h3 className='errMsg'>{errorMessage}</h3> */}
-        <Form onSubmit={handleSubmit}   style={{width:'400px',border:'1px solid',padding:'30px'}}>
-         <h2>AdminLogin</h2>
+      <div className="container-adminLogin">
+        
+        <div className="form-container fade-in-animation">
+          <Form onSubmit={handleSubmit}>
+            <h2>Admin Login</h2>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={Email}
+                onChange={handleEmail}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicEmail" >
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"                                           
-              placeholder="Enter email"
-              value={Email}
-              onChange={handleEmail}
-            />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={Password}
+                onChange={handlePassword}
+              />
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={Password}
-              onChange={handlePassword}
-            />
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
 
       <Modal show={showErrorModal} onHide={handleCloseErrorModal}>
@@ -103,7 +91,7 @@ const AdminLogin = () => {
           <Modal.Title>Error</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p style={{color:'red'}}>{errorMessage}</p>
+          <p style={{ color: 'red' }}>{errorMessage}</p>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseErrorModal}>
@@ -112,7 +100,6 @@ const AdminLogin = () => {
         </Modal.Footer>
       </Modal>
     </div>
-
   );
 };
 
--- BookStore/client/src/Users/ViewCart.jsx
+++ BookStore/client/src/Users/ViewCart.jsx
@@ -1,40 +1,40 @@
 
 
-
-
-
-
-
-
-
-
 import axios from 'axios';
 import React, { useState, useEffect, useContext } from 'react';
 import './ViewCart.css';
 import { BiRupee } from 'react-icons/bi';
 import { IoTrashBin } from 'react-icons/io5';
-import { Table } from 'react-bootstrap'
-import jwt_decode from "jwt-decode";
+import { Table, Button } from 'react-bootstrap'; // Import Button from react-bootstrap
+import jwt_decode from 'jwt-decode';
 import { newContext } from './Contexts/ViewCartContext';
 import ShippingAddressForm from './ShippingAddressForm';
 import { useNavigate } from 'react-router-dom';
 
 const ViewCart = () => {
   const [cartItems, setCartItems] = useState([]);
-  const [totalPrize, setTotalPrize] = useState(0)
-  const [UserId, setUserId] = useState(null)
-  const {shippingAddress,setShippingAddress} = useContext(newContext);
-  const [showShippingAddressForm, setShowShippingAddressForm] = useState(false)
-
+  const [totalPrize, setTotalPrize] = useState(0);
+  const [UserId, setUserId] = useState(null);
+  const { shippingAddress, setShippingAddress } = useContext(newContext);
+  const [showShippingAddressForm, setShowShippingAddressForm] = useState(false);
 
   useEffect(() => {
     viewBook();
   }, []);
 
-
   useEffect(() => {
     calculateTotalPrize();
-  }, [cartItems])
+  }, [cartItems]);
+
+  useEffect(() => {
+    const token = localStorage.getItem('userInfooooo');
+    if (token) {
+      const decodedToken = jwt_decode(token);
+      const userId = decodedToken.id;
+      console.log('decodedToken.id:::::', decodedToken.id);
+      setUserId(userId);
+    }
+  }, []);
 
   const viewBook = async () => {
     try {
@@ -43,37 +43,22 @@ const ViewCart = () => {
     } catch (error) {
       console.log('Error fetching book:', error);
     }
-  }
-
-  const alertMessage = () => {
-    alert("Count cannot be less than one");
-  }
-
-  const updateItemCount = async (itemId, newCount) => {
-    try {
-      const item = cartItems.find(item => item._id === itemId);
-      if (item) {
-        const updatedPrize = item.InitialPrize * newCount;
-        const response = await axios.put(`http://localhost:5000/cartCountUpdate/${itemId}`, { Count: newCount, Prize: updatedPrize });
-        if (response.status === 200) {
-          // Update the cartItems array with the new count and updated prize
-          setCartItems(prevCartItems =>
-            prevCartItems.map(item => (item._id === itemId ? { ...item, Count: newCount, Prize: updatedPrize } : item))
-          );
-        }
-      }
-    } catch (error) {
-      console.log('Error updating item count:', error);
-    }
   };
 
+  const calculateTotalPrize = async () => {
+    let totalPrize = 0;
+    cartItems.forEach((item) => {
+      totalPrize += item.InitialPrize * item.Count;
+    });
+    setTotalPrize(totalPrize);
+  };
 
   const decrementCount = async (itemId) => {
-    const item = cartItems.find(item => item._id === itemId);
+    const item = cartItems.find((item) => item._id === itemId);
     if (item) {
       const newCount = item.Count - 1;
       if (newCount >= 0) {
-        const updatedPrize = item.InitialPrize * newCount; // Calculate the updated prize
+        const updatedPrize = item.InitialPrize * newCount;
         await updateItemCount(itemId, newCount, updatedPrize);
       } else {
         alertMessage();
@@ -82,133 +67,72 @@ const ViewCart = () => {
   };
 
   const incrementCount = async (itemId) => {
-    const item = cartItems.find(item => item._id === itemId);
+    const item = cartItems.find((item) => item._id === itemId);
     if (item) {
       const newCount = item.Count + 1;
-      const updatedPrize = item.InitialPrize + item.Prize * newCount; // Calculate the updated prize
+      const updatedPrize = item.InitialPrize * newCount;
       await updateItemCount(itemId, newCount, updatedPrize);
     }
   };
 
+  const updateItemCount = async (itemId, newCount, updatedPrize) => {
+    try {
+      const response = await axios.put(`http://localhost:5000/cartCountUpdate/${itemId}`, {
+        Count: newCount,
+        Prize: updatedPrize,
+      });
+      if (response.status === 200) {
+        setCartItems((prevCartItems) =>
+          prevCartItems.map((item) => (item._id === itemId ? { ...item, Count: newCount, Prize: updatedPrize } : item))
+        );
+      }
+    } catch (error) {
+      console.log('Error updating item count:', error);
+    }
+  };
 
   const deleteCart = async (cartItemId) => {
     try {
-      const response = await axios.delete(`http://localhost:5000/cart/${cartItemId}`)
-      console.log('Succcessfully deleted :', response.data);
-      setCartItems((prevCartItems) => prevCartItems.filter(item => item._id !== cartItemId))
+      const response = await axios.delete(`http://localhost:5000/cart/${cartItemId}`);
+      console.log('Successfully deleted:', response.data);
+      setCartItems((prevCartItems) => prevCartItems.filter((item) => item._id !== cartItemId));
     } catch (error) {
-      console.log('Error on Deleting Cart Item :', error);
+      console.log('Error on Deleting Cart Item:', error);
     }
-  }
+  };
 
-
-  const calculateTotalPrize = async () => {
-    let totalPrize = 0;
-    cartItems.forEach(item => {
-      totalPrize += item.InitialPrize * item.Count
-    });
-    setTotalPrize(totalPrize)
-  }
-
-
-  useEffect(() => {
-    const token = localStorage.getItem("userInfooooo")
-    if (token) {
-      const decodedToken = jwt_decode(token)
-      const userId = decodedToken.id
-      console.log("decodedToken.id:::::", decodedToken.id)
-      setUserId(userId)
-
-    }
-  }, [])
+  const alertMessage = () => {
+    alert("Count cannot be less than one");
+  };
 
   const toggelShippingAddressForm = () => {
-    setShowShippingAddressForm(!showShippingAddressForm)
-  }
+    setShowShippingAddressForm(!showShippingAddressForm);
+  };
 
-
-  const navigate = useNavigate()
-
-  // const userOrder = async () => {
-  //   console.log('userOrder:::',UserId,cartItems,totalPrize,shippingAddress);
-  //   try {
-      
-      
-
-  //     const response = await axios.post('http://localhost:5000/userOrderr',{UserId,cartItems,totalPrize,shippingAddress:{...shippingAddress}} );
-  //     console.log('zzzzzzzzzz:::',response.data);
-  //     console.log('cccccccart::',response.data.items.map((obj)=> obj.product));
-
-  //     if (response.data) {
-  //       const deleteCartItemsAftrSuccssOrder = response.data.items.map((obj)=> obj._id)
-  //       // let product = response.data.items.find((value)=>value.product)
-  //       // console.log('proooduct:::',response.data.items.find((value)=>value.product));
-  //       // Delete cart items one by one using the deleteCart function
-  //       for(let _id of deleteCartItemsAftrSuccssOrder){
-  //         // await deleteCart(product)
-  //         await axios.delete(`http://localhost:5000/cart/${_id}`);
-  //       }
-        
-        
-  //       // setCartItems([]);
-  //       navigate('/placeOrderSuccess');
-  //     }
-      
-  //   } catch (error) {
-  //     console.log('Error on Ordering Products.....', error);
-  //     // console.log('Response Data:', error.response.data); // Log response data if available
-      
-  //   }
-  // };
-
-
+  const navigate = useNavigate();
 
   const userOrder = async () => {
     console.log('userOrder:::', UserId, cartItems, totalPrize, shippingAddress);
     try {
-
-
-      // const orderData = {
-      //   userId: UserId,
-      //   items: cartItems,
-      //   total: totalPrize,
-      //   shippingAddress: {...shippingAddress},
-      //   paymentStatus: 'pending',
-      //   orderDate: new Date().toISOString()
-      // };
-
-      // const orderData = {
-      //   userId: UserId,
-      //   items: cartItems,
-      //   total: totalPrize,
-      //   shippingAddress: {
-      //     address: shippingAddress.address,
-      //     city: shippingAddress.city,
-      //     mobile: shippingAddress.mobile,
-      //   },
-      //   paymentStatus: 'pending',
-      //   orderDate: new Date().toISOString()
-      // };
-
       const response = await axios.post('http://localhost:5000/userOrderr', {
         UserId,
         cartItems,
         totalPrize,
-        shippingAddress: { ...shippingAddress }
+        shippingAddress: { ...shippingAddress },
       });
       console.log('Response after ordering:', response.data);
-  
+
       if (response.data) {
         const deleteCartItemsAftrSuccssOrder = response.data.items.map((obj) => obj._id);
         console.log('Delete cart items:', deleteCartItemsAftrSuccssOrder);
-  
+
         for (let _id of deleteCartItemsAftrSuccssOrder) {
           console.log('Deleting cart item with _id:', _id);
           await axios.delete(`http://localhost:5000/cart/${_id}`);
-          setCartItems((prevCartItems)=> prevCartItems.filter(item => item._id !== _id))
+          setCartItems((prevCartItems) => prevCartItems.filter((item) => item._id !== _id));
           console.log('Deleted cart item with _id:', _id);
         }
-  
+
         console.log('Cart items deleted');
         navigate('/placeOrderSuccess');
       }
@@ -216,15 +140,10 @@ const ViewCart = () => {
       console.log('Error on Ordering Products:', error);
     }
   };
-  
-
-
-
 
   return (
     <div className='container'>
-      {/* <ShippingAddressForm  shippingAddress={shippingAddress} onShippingAddressChange={setShippingAddress}  /> */}
-      <Table striped bordered hover>
+      <Table striped bordered hover responsive>
         <thead>
           <tr>
             <th>Product</th>
@@ -243,13 +162,13 @@ const ViewCart = () => {
                 {item.Prize}
               </td>
               <td>
-                <button className='btn btn-primary' onClick={() => decrementCount(item._id)}>
+                <Button variant='primary' onClick={() => decrementCount(item._id)}>
                   -
-                </button>
+                </Button>
                 {item.Count}
-                <button className='btn btn-primary' onClick={() => incrementCount(item._id)}>
+                <Button variant='primary' onClick={() => incrementCount(item._id)}>
                   +
-                </button>
+                </Button>
               </td>
               <td>
                 <BiRupee size={16} />
@@ -262,15 +181,20 @@ const ViewCart = () => {
           ))}
         </tbody>
       </Table>
-      <div className='totalPrize' style={{ display: 'flex' }}>
+      <div className='totalPrize'>
         <h3>
           Total Price: <BiRupee size={24} />
           {totalPrize}
         </h3>
-        {/* <button onClick={userOrder} className='orderBtn btn btn-success '>Order Now</button> */}
-        <button onClick={toggelShippingAddressForm} className='orderBtn btn btn-success '>Order Now</button>
+        <Button onClick={toggelShippingAddressForm} variant='success'>
+          Order Now
+        </Button>
         {showShippingAddressForm && (
-          <ShippingAddressForm  shippingAddress={shippingAddress} onShippingAddressChange={setShippingAddress} onSubmit={userOrder} />
+          <ShippingAddressForm
+            shippingAddress={shippingAddress}
+            onShippingAddressChange={setShippingAddress}
+            onSubmit={userOrder}
+          />
         )}
       </div>
     </div>
@@ -278,175 +202,3 @@ const ViewCart = () => {
 };
 
 export default ViewCart;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import axios from 'axios';
-// import React, { useState, useEffect } from 'react';
-// import { useParams } from 'react-router-dom';
-// import './ViewCart.css';
-// import { BiRupee } from 'react-icons/bi';
-// import { IoTrashBin } from 'react-icons/io5';
-
-// const ViewCart = () => {
-//   const [cartItems, setCartItems] = useState([]);
-//   const { bookId } = useParams();
-
-//   useEffect(() => {
-//     viewBook();
-//   }, []);
-
-//   const viewBook = async () => {
-//     try {
-//       const response = await axios.get(`http://localhost:5000/cart`);
-//       setCartItems(response.data);
-//     } catch (error) {
-//       console.log('Error fetching book:', error);
-//     }
-//   }
-
-//   const updateItemCount = async (itemId, newCount) => {
-//     try {
-//       // Calculate the updated prize based on the new count
-//       const item = cartItems.find(item => item._id === itemId);
-//       if (item) {
-//         const initialPrize = item.Prize; 
-//         // const updatedPrize = initialPrize + initialPrize;
-//         const updatedPrize = initialPrize * newCount;
-//         await axios.put(`http://localhost:5000/cartCountUpdate/${itemId}`, { Count: newCount, Prize: updatedPrize });
-//         // new count and updated Prize vech cartItems array  ne update cheyyan
-//         setCartItems(prevCartItems =>
-//           prevCartItems.map(item => (item._id === itemId ? { ...item, Count: newCount, Prize: updatedPrize } : item))
-//         );
-//       }
-//     } catch (error) {
-//       console.log('Error updating item count:', error);
-//     }
-//   };
-
-
-//   const decrementCount = async (itemId) => {
-//     const item = cartItems.find(item => item._id === itemId);
-//     if (item) {
-//       const newCount = item.Count - 1;
-//       if (newCount >= 0) {
-//         await updateItemCount(itemId, newCount);
-//       } else {
-//         alert("Count cannot be less than one");
-//       }
-//     }
-//   };
-
-//   const incrementCount = async (itemId) => {
-//     const item = cartItems.find(item => item._id === itemId);
-//     if (item) {
-//       const newCount = item.Count + 1;
-//       await updateItemCount(itemId, newCount);
-//     }
-//   };
-
-
-// const deleteCart = async (cartItemId) => {
-//   try {
-//     const response = await axios.delete(`http://localhost:5000/cart/${cartItemId}`)
-//     console.log('Succcessfully deleted :',response.data);
-//     setCartItems((prevCartItems) => prevCartItems.filter(item => item._id ==! cartItemId))
-//   } catch (error) {
-//       console.log('Error on Deleting Cart Item :',error);
-//   }
-
-// }
-
-//   return (
-//     <div className='container'>
-//       {cartItems.map((item) => (
-//         <div key={item._id}>
-//           <h1>Name: {item.Name}</h1>
-//           <div style={{ display: 'flex', alignItems: 'center' }}>
-//             <BiRupee size={24} />
-//             <h3>{item.Prize}</h3>
-//             <div style={{ display: 'flex', alignItems: 'center', marginLeft: '32%', marginTop: '0px' }}>
-//               <button className='btn btn-primary' onClick={() => decrementCount(item._id)}>
-//                 -
-//               </button>
-//               <h3>{item.Count}</h3>
-//               <button className='btn btn-primary' onClick={() => incrementCount(item._id)}>
-//                 +
-//               </button>
-
-//               <IoTrashBin onClick={() => deleteCart(item._id)} className='delete-icon'/>
-
-//             </div>
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default ViewCart;
-
-
-
-
-
-
-
-
-
-
-
-
